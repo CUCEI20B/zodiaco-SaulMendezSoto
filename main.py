856dcaf0bdb695358d4c2d1a90be67dca25c075b
Saul Mendez <saul.mendez5889@alumnos.udg.mx>
2020-09-30 11:22:01 -0700

    prueba 01

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print("Hola Mundo")
+dia = int(input("Ingresa  tu dia de nacimiento para saber tu signo: "))
+mes = int(input("Ingresa tu mes de nacimiento para saber tu signo: "))
+
+if dia >= 22 and mes == 12 or dia <= 20 and mes == 1:
+    print("Tu signo zodiacal es: capricornio")
+if dia >= 21 and mes == 1 or dia <= 19 and mes == 2:
+    print("Tu signo zodiacal es: acuario")
+if dia >= 20 and mes == 2 or dia <= 20 and mes == 3:
+    print("Tu signo zodiacal es: picis")
+if dia >= 21 and mes == 3 or dia <= 20 and mes == 4:
+    print("Tu signo zodiacal es: aries")
+if dia >= 21 and mes == 4 or dia <= 21 and mes == 5:
+    print("Tu signo zodiacal es: tauro")
+if dia >= 22 and mes == 5 or dia <= 21 and mes == 6:
+    print("Tu signo zodiacal es: geminis") 
+if dia >= 22 and mes == 6 or dia <= 23 and mes == 7:
+    print("Tu signo zodiacal es: cancer")
+if dia >= 24 and mes == 7 or dia <= 23 and mes == 8:
+    print("Tu signo zodiacal es: leo")
+if dia >= 24 and mes == 8 or dia <= 23 and mes == 9:
+    print("Tu signo zodiacal es: virgo")
+if dia >= 24 and mes == 9 or dia <= 23 and mes == 10:
+    print("Tu signo zodiacal es: libra")
+if dia >= 24 and mes == 10 or dia <= 22 and mes == 11:
+    print("Tu signo zodiacal es: escorpio")
+if dia >= 23 and mes == 11 or dia <= 21 and mes == 12:
+    print("Tu signo zodiacal es: sagitario")
+
+   
